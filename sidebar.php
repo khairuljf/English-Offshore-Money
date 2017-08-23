912a431f6bf0658d4bf2ee27c52763c465bc658f
Khairul Islam <khairuljf@gmail.com>
2017-08-23 17:21:26 +0600

    create sidebar for contact form

---
+++ sidebar.php
@@ -0,0 +1,33 @@
+<aside id="sidebar">
+    <div class="side-quote-form">
+        <form name="form1" method="POST" class="registration-form" id="quoteForm" onSubmit="return false;">
+            <fieldset>
+                <input type="hidden" value="1" name="validate">
+                <span class="free-text">Get Free Quote Today and Start Saving</span>
+                <label for="company">BUSINESS NAME<span>*</span></label>
+                <input type="text" name="company" class="required" id="company" required>
+                <label for="first_name">YOUR NAME<span>*</span></label>
+                <input type="text" name="first_name" class="required" id="first_name" required>
+                <label for="last_name">YOUR LAST NAME<span>*</span></label>
+                <input type="text" name="last_name" class="required" id="last_name" required>
+                <label for="phone">TELPHONE NUMBER<span>*</span></label>
+                <input type="text" name="phone" class="required" id="phone" required>
+                <label for="mobile">WHAT SERVICE ARE YOU LOOKING FOR?<span>*</span></label>
+                <input type="text" name="mobile" class="required" id="mobile" required>
+                <label for="email">Email Address<span>*</span></label>
+                <input type="text" name="email" class="email required-email" id="email" required>
+
+
+                <div style="width: 235px; margin-left: 20px;">
+                    <div style="transform:scale(0.77);-webkit-transform:scale(0.77);transform-origin:0 0;-webkit-transform-origin:0 0;" class="g-recaptcha" data-sitekey="6LfhdSUTAAAAAK98B5DexzUQ25n9RaXHzTfibkey"></div>
+                </div>
+                <div class="submit-wrap">
+                    <input type="submit" id="btnSubmit" value="GET FREE QUOTE!">
+                </div>
+                <span class="out-shadow"></span>
+            </fieldset>
+        </form>
+    </div>
+    <!-- products --> 
+
+</aside>
